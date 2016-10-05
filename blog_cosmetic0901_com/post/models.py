c3fb8cd495c2cf9ec9f650f7709c31d0bb6e7a0a
LEESM <fast0522@gmail.com>
2016-10-06 02:08:12 +0900

    update1

---
+++ blog_cosmetic0901_com/post/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.conf import settings
+
+class Category(models.Model):
+	category_name = models.CharField(max_length=50)
+	order = models.IntegerField(default=0)
+
+	def __str__(self):
+		return self.category_name
+
+def get_image_path(instance, filename):
+	return os.path.join('item_images', str(instance.item_id), filename)
+
+class Item(models.Model):
+	subject = models.CharField(max_length=100)
+	description = models.CharField(max_length=100,blank=True)
+	content = models.TextField(blank=True)
+	image = models.ImageField(blank=True, upload_to=get_image_path)
+	author = models.ForeignKey(settings.AUTH_USER_MODEL)
+	category = models.ForeignKey(Category,blank=True, null=True,on_delete=models.SET_NULL)
+	pub_date = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.subject
