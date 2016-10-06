7e9dd376df5d26e69cddc5326db7f881cabc9122
LEESM <fast0522@gmail.com>
2016-10-06 17:05:18 +0900

    wsgi setting modified

--- blog_cosmetic0901_com/blog_cosmetic0901_com/wsgi.py
+++ blog_cosmetic0901_com/blog_cosmetic0901_com/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog_cosmetic0901_com.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog_cosmetic0901_com.settings.prod")
 
 application = get_wsgi_application()
