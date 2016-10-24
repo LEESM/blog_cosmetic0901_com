fb673700fddae814c05b8ffe113fa43603ccb925
LEESM <fast0522@gmail.com>
2016-10-24 17:58:49 +0900

    update

---
+++ blog_cosmetic0901_com/static_local/js/base.js
@@ -0,0 +1,4 @@
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+	$("#main_content img").addClass("img-responsive");
+	$("#main_content img").attr("style","width:100%;");
+}
